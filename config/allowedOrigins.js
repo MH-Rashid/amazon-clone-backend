c7bf19122fa9b9861bd1979d3c6579f43e86c3d5
MH-Rashid <morashid99@hotmail.com>
2025-08-30 23:08:45 +0100

    updated allowed origins to allow deployed frontend to connect

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://www.google.com", // to access the site via Google search results
   "http://127.0.0.1:5500",
-  "https://mh-rashid.github.io/amazon-clone"
+  "https://amazon-clone-frontend-lesm.onrender.com"
 ];
 
 module.exports = allowedOrigins;
